ad3f23c3755bb5e9f947f0650c175b8c6cce664a
Slepova Ludmila <slepova@radario.co>
2020-04-17 15:46:34 +0300

    login page validation

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,7 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    REGISTER_BUTTON = (By.CSS_SELECTOR, '[name="registration_submit"]')
+    LOGIN_BUTTON = (By.CSS_SELECTOR, '[name="login_submit"]')
--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,25 @@
+import pytest
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      
-    page.go_to_login_page()    
+    page.go_to_login_page() 
+    page_log = LoginPage(browser, url=browser.current_url)
+    page_log.open()
+    page_log.should_be_login_page()   
 
 def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
-
+    # 
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
