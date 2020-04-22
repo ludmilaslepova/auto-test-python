a72018c94c103c07820c137c1a0d0c7648cbd8b3
Slepova Ludmila <slepova@radario.co>
2020-04-22 16:26:16 +0300

    added new class

--- test_main_page.py
+++ test_main_page.py
@@ -4,26 +4,27 @@ from pages.login_page import LoginPage
 from pages.base_page import BasePage
 from pages.basket_page import BasketPage
 
-@pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      
-    page.go_to_login_page() 
-    page_log = LoginPage(browser, url=browser.current_url)
-    page_log.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+        page.open()                      
+        page.go_to_login_page() 
+        page_log = LoginPage(browser, url=browser.current_url)
+        page_log.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
 
 @pytest.mark.skip
 def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
-
-@pytest.mark.skip
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
 
 @pytest.mark.xfail
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
